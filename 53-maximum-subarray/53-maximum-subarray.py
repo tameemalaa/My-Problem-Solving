b827a99496bb2e1f808c3df4b11af26f796beb2f
tameemalaa <tameem.alaa@gmail.com>
2022-06-14 14:54:14 +0200

    Time: 1219 ms (22.39%), Space: 27.9 MB (38.84%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,15 +1,11 @@
-
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        global_max = -10**5
-        local_max = 0
-        for i in range(len(nums)) : 
-            local_max += nums[i]
-            global_max= max((local_max , global_max))
-            if (local_max <0) : local_max = 0
-        return global_max
-                
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        summ = 0
+        g_max = float('-inf')
+        
+        for num in nums:
+            summ+= num
+            g_max = max(g_max,summ)
+            if summ <0: summ=0
+        return g_max
+        
