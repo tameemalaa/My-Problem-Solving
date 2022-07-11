0623854867293b7fb7defab58301a2f257e722ba
tameemalaa <tameem.alaa@gmail.com>
2022-07-11 04:17:10 +0200

    Time: 250 ms (81.49%), Space: 25.4 MB (75.31%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def rotate(self, nums, k):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
         x = nums[:]
-        print(x)
-        print(nums)
         for i in range(len(nums)):
             nums[(i+k)%len(nums)]=x[i]
-        
