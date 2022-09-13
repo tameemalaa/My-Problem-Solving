d0e497738a32d9d98ca04cebb8b23f8be696b9c4
tameemalaa <tameem.alaa@gmail.com>
2022-09-13 16:04:52 +0200

    Time: 25 ms (98.67%), Space: 14 MB (11.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,9 @@
-z = {1:1,
-    2:2}
 class Solution:
     def climbStairs(self, n: int) -> int:
+        z = {1:1,2:2}
+        def sol(n):
+            if n in z.keys():
+                return z[n]
+            z[n] = sol(n-1) + sol(n-2)
+            return z[n]
         return sol(n)
-        
-def sol(n):
-    if n in z.keys():
-        return z[n]
-    z[n] = sol(n-1) + sol(n-2)
-    return z[n]
